8e02079f8bcb372293a665e4e547e9417cbbf792
Chris Lambacher <chris@kateandchris.net>
2011-09-20 16:42:11 +0000

    Connection framework
    git-svn-id: https://thatsmymeatball.com/svn/cioc/featuredb/trunk@3643 849ea9ae-eb3e-43d2-a8ab-95ee78932fa3

--- featuredb/__init__.py
+++ featuredb/__init__.py
@@ -1,22 +1,49 @@
+import os
+
+from featuredb import config, connection
+
 from pyramid.config import Configurator
 from pyramid_beaker import session_factory_from_settings
 
+app_path = None
+config_file = None
+app_name = None
+
+def on_new_request(event):
+	global app_path, config_file, app_name
+	request = event.request
+
+	#renderer = get_renderer('cioc.web:templates/master.mak')
+	
+	#set_template_lookup(request.registry.queryUtility(IMakoLookup))
+
+	if app_path is None:
+		app_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '..', '..'))
+		app_name = os.path.split(app_path)[1]
+		config_file = os.path.join(app_path, '..', 'config', app_name + '.ini')
+
+	request.config = config.get_config(config_file)
+	request.connmgr = connection.ConnectionManager(request, request.config)
+
+
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
-    session_factory = session_factory_from_settings(settings)
-    config = Configurator(settings=settings, session_factory=session_factory)
+	""" This function returns a Pyramid WSGI application.
+	"""
+	session_factory = session_factory_from_settings(settings)
+	config = Configurator(settings=settings, session_factory=session_factory)
 
-    config.add_static_view('static', 'featuredb:static', cache_max_age=3600)
-    config.add_static_view('favicon.ico', 'featuredb:static/favicon.ico', cache_max_age=3600)
-    config.add_static_view('robots.txt', 'featuredb:static/robots.txt', cache_max_age=3600)
-    config.add_static_view('humans.txt', 'featuredb:static/humans.txt', cache_max_age=3600)
+	config.add_static_view('static', 'featuredb:static', cache_max_age=3600)
+	config.add_static_view('favicon.ico', 'featuredb:static/favicon.ico', cache_max_age=3600)
+	config.add_static_view('robots.txt', 'featuredb:static/robots.txt', cache_max_age=3600)
+	config.add_static_view('humans.txt', 'featuredb:static/humans.txt', cache_max_age=3600)
 
-    config.add_handler('home_index', '/',
-            handler='featuredb.views.Index', action='index')
+	config.add_handler('home_index', '/',
+			handler='featuredb.views.Index', action='index')
 
-    config.add_handler('home', '/{action}',
-            handler='featuredb.views.Index')
+	config.add_handler('home', '/{action}',
+			handler='featuredb.views.Index')
 
-    return config.make_wsgi_app()
+	config.add_subscriber(on_new_request, 'pyramid.events.NewRequest')
+
+	return config.make_wsgi_app()
 
---
+++ featuredb/connection.py
@@ -0,0 +1,32 @@
+# =================================================================
+# Copyright (C) 2011 Community Information Online Consortium (CIOC)
+# http://www.cioc.ca
+# Developed By Katherine Lambacher / KCL Custom Software
+# If you did not receive a copy of the license agreement with this
+# software, please contact CIOC via their website above.
+#==================================================================
+import pyodbc
+
+class ConnectionManager(object):
+	def __init__(self, request, config):
+		self.request = request
+		self.config= config
+
+		
+	def get_connection_string(self):
+		config = self.config
+		settings = [
+			('Driver', '{SQL Server Native Client 10.0}'),
+			('Server', config['server']),
+			('Database', config['database']),
+			('UID', config['uid']),
+			('PWD', config['pwd'])
+		]
+
+		return ';'.join('='.join(x) for x in settings)
+
+	def get_connection(self):
+		conn = pyodbc.connect(self.get_connection_string(), autocommit=True, unicode_results=True)
+
+		return conn
+
