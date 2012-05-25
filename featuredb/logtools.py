5fb6ceef184a5737f34f7b4bd4b52a9a7e5d5ab3
Chris Lambacher <chris@kateandchris.net>
2012-05-25 18:49:01 +0000

    Always date log files.
    git-svn-id: https://thatsmymeatball.com/svn/cioc/featuredb/trunk@4510 849ea9ae-eb3e-43d2-a8ab-95ee78932fa3

--- featuredb/logtools.py
+++ featuredb/logtools.py
@@ -1,4 +1,4 @@
-import os
+import os, time
 import logging.handlers
 
 _app_name = None
@@ -13,17 +13,67 @@ def _get_app_name():
 
 _log_root = None
 class TimedRotatingFileHandler(logging.handlers.TimedRotatingFileHandler):
+	"""
+	A version of logging.handlers.TimedRotatingFileHandler that knows about the 
+	location to store log files and calculates the path based on the app name. It also 
+	always stores to a dated filename.
+	"""
 	def __init__(self, name):
 		global _log_root
 
 		app_name = _get_app_name()
 
 		if _log_root is None:
-			_log_root = os.environ.get('CIOC_LOG_ROOT', 'd:\log')
+			_log_root = os.environ.get('CIOC_LOG_ROOT', 'd:\logs')
 
-		logfile = os.path.join(_log_root, app_name, 'python', name)
+		self._logfilename = os.path.join(_log_root, app_name, 'python', name)
 
-		logging.handlers.TimedRotatingFileHandler.__init__(self, logfile, 'midnight', delay=True)
+		logging.handlers.TimedRotatingFileHandler.__init__(self, self._logfilename, 'midnight', delay=True)
+
+		t = self.rolloverAt - self.interval
+		if self.utc:
+			timeTuple = time.gmtime(t)
+		else:
+			timeTuple = time.localtime(t)
+
+		self.baseFilename = os.path.abspath(self._logfilename + '.' + time.strftime(self.suffix, timeTuple))
+
+	def doRollover(self):
+		"""
+		do a rollover; in this case, a date/time stamp is appended to the filename
+		when the rollover happens.	However, you want the file to be named for the
+		start of the interval, not the current time.  If there is a backup count,
+		then we have to get a list of matching filenames, sort them and remove
+		the one with the oldest suffix.
+		"""
+		t = self.rolloverAt - self.interval
+		if self.utc:
+			timeTuple = time.gmtime(t)
+		else:
+			timeTuple = time.localtime(t)
+
+		self.baseFilename = os.path.abspath(self._logfilename + '.' + time.strftime(self.suffix, timeTuple))
+
+		if self.stream:
+			self.stream.close()
+			self.stream = None
+
+		self.mode = 'w'
+		self.stream = self._open()
+		currentTime = int(time.time())
+		newRolloverAt = self.computeRollover(currentTime)
+		while newRolloverAt <= currentTime:
+			newRolloverAt = newRolloverAt + self.interval
+		#If DST changes and midnight or weekly rollover, adjust for this.
+		if (self.when == 'MIDNIGHT' or self.when.startswith('W')) and not self.utc:
+			dstNow = time.localtime(currentTime)[-1]
+			dstAtRollover = time.localtime(newRolloverAt)[-1]
+			if dstNow != dstAtRollover:
+				if not dstNow:	# DST kicks in before next rollover, so we need to deduct an hour
+					newRolloverAt = newRolloverAt - 3600
+				else:			# DST bows out before next rollover, so we need to add an hour
+					newRolloverAt = newRolloverAt + 3600
+		self.rolloverAt = newRolloverAt
 
 
 _server = None
