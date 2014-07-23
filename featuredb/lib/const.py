b651369aab08e5cac17fef6350f346b2ac73bb8f
Christopher Lambacher <chris@kateandchris.net>
2014-07-23 15:29:48 -0400

    Make cache values more resilient

--- featuredb/lib/const.py
+++ featuredb/lib/const.py
@@ -4,16 +4,16 @@
 # Developed By Katherine Lambacher / KCL Custom Software
 # If you did not receive a copy of the license agreement with this
 # software, please contact CIOC via their website above.
-#==================================================================
+# ==================================================================
 
-#stdlib 
+# stdlib
 import os
 
-#jQuery and jQueryUI versions
+# jQuery and jQueryUI versions
 JQUERY_VERSION = "1.6.2"
 JQUERY_UI_VERSION = "1.8.16"
 
-#formatting constants
+# formatting constants
 DATE_TEXT_SIZE = 25
 TEXT_SIZE = 85
 TEXTAREA_COLS = 85
@@ -29,13 +29,13 @@ _config_file = None
 _app_name = None
 session_lock_dir = None
 
+
 def update_cache_values():
     # called from application init at startup
     global _app_path, _config_file, _app_name, session_lock_dir, publish_dir
 
-
     if _app_path is None:
-        _app_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '..', '..'))
+        _app_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..'))
         _app_name = os.path.split(_app_path)[1]
         _config_file = os.path.join(_app_path, '..', '..', 'config', _app_name + '.ini')
         session_lock_dir = os.path.join(_app_path, 'session_lock')
